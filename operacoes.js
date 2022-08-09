84e376a03700c846c49599ab35317dba63857405
Gabriel Oliveira da Silva <91036428+GabrielOliveiraDaSilva@users.noreply.github.com>
2022-08-09 12:52:57 -0300

    Operações Básicas de Matemática

---
+++ operacoes.js
@@ -0,0 +1,38 @@
+//Neste arquivo contém as operações matemáticas básicas.
+
+// Operação de soma.
+
+let somaNum1 = 1;
+let somaNum2 = 3;
+
+const soma = somaNum1 + somaNum2;
+
+console.log(soma);
+
+// Operação de subtração.
+
+let subNum1 = 5;
+let subNum2 = 4;
+
+const subtracao = subNum1 - subNum2;
+
+console.log(subtracao);
+
+// Operação de divisão.
+
+let divNum1 = 10;
+let divNum2 = 2;
+
+const divisao = divNum1 / divNum2;
+
+console.log(divisao);
+
+//Operação de multiplicação.
+
+let multNum1 = 9;
+let multNum2 = 9;
+
+const multiplicacao = multNum1 * multNum2;
+
+console.log(multiplicacao);
+
